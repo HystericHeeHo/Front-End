8fe47a48998137000129ee6d117f0f4fcaeb4794
Justin <justintadams22@gmail.com>
2017-09-19 21:12:31 +0000

    created second script to have user choose username and have head change based off username

--- scripts/nameChange.js
+++ scripts/nameChange.js
@@ -1,12 +1,19 @@
 var myButton = document.querySelector('button');
 var myHeading = document.querySelectot('h1');
-function setUserName = (){
-    var myName = prompt('Please Enter Your Name:');
-    localStorage.setItem('name', myName);
-    if(!localStorage.getItem('name')) {
-    setUserName();
-} 
-    else {
+
+function setUserName() {
+  var myName = prompt('Please enter your name:')
+  localStorage.setItem('name', myName);
+  myHeading.textcontect = 'Welcome to Learning about tarot arcanas,' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+  } else {
     var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Are you here to learn about tarot cards,' + storedName;
+    myHeading.textcontent = 'Welcome to Learning about tarot arcanas,' + storedName;
+  }
+  
+  myButton.onclick = function() {
+  setUserName();
 }
