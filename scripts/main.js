efb41ae225b9bfedd79421480087568de4c290ff
Justin <justintadams22@gmail.com>
2017-09-19 21:25:04 +0000

    cworking version of the script

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/emperor.jpg');
     }
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name:')
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to Learning about tarot arcanas,' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome to Learning about tarot arcanas,' + storedName;
+  }
+  
+  myButton.onclick = function() {
+  setUserName();
+}
